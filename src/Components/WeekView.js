6f7f18e90212745979502b38474ccc5f6398d9f4
Joscelyn James <josce.james7@hotmail.com>
2019-01-29 22:51:09 -0700

    rows expand and collapse on click

--- src/Components/WeekView.js
+++ src/Components/WeekView.js
@@ -5,20 +5,35 @@ const WeekView = ({ weeklyForecast }) => {
   return (
     <div className="week-view-container">
       <table>
-        <tbody>
-          {weeklyForecast.map(day => {
-            const date = getDateFromEpoch(day.time)
-            return (
-              <tr key={day.time}>
-                <th className="day-of-week">{getDayOfWeek(date)}</th>
+        {weeklyForecast.map((day, i) => {
+          const date = getDateFromEpoch(day.time)
+          return (
+            <tbody key={day.time}>
+              <tr onClick={e => handleClick(e)}>
+                <th className={i + ' day-of-week'}>{getDayOfWeek(date)}</th>
                 <th>{day.temperatureMax.toFixed(0)}°</th>
               </tr>
-            )
-          })}
-        </tbody>
+              <tr className={i + ' hide-day-info'}>
+                <th>this is day info lalalala</th>
+              </tr>
+            </tbody>
+          )
+        })}
       </table>
     </div>
   )
+}
+
+const handleClick = e => {
+  const elements = document.getElementsByClassName(e.target.classList[0])
+
+  if (elements[1].classList.contains('hide-day-info')) {
+    elements[1].classList.remove('hide-day-info')
+    elements[1].classList.add('show-day-info')
+  } else {
+    elements[1].classList.remove('show-day-info')
+    elements[1].classList.add('hide-day-info')
+  }
 }
 
 export default WeekView
